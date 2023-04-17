f506c8ec146b581f2e8768abd783ff6c0e2c9741
Eslim Daga <esliim9811@gmail.com>
2023-04-16 23:02:52 -0500

    config:🔧:Eh configurado las primeras rutas, iniciar-sesion y registrarse

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import SignIn from "./views/authentication/signin";
 import "./assets/css/main.css";
+import SignUp from "./views/authentication/signup";
+
+const router = createBrowserRouter([
+	{
+		path: "/iniciar-sesion",
+		element: <SignIn />,
+	},
+	{
+		path: "/registrarse",
+		element: <SignUp />,
+	}
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
