953c87f6b9d691906226522609db5d838a5f8b08
Eslim Daga <esliim9811@gmail.com>
2023-04-17 22:01:32 -0500

    update:💄:Eh agregado un nuevo color platzi-secondary-purple.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
 				"platzi-primary-background": "#13161c",
 				"platzi-primary-green": "#0ae98a",
 				"platzi-primary-purple": "#4d1bd1",
+				"platzi-secondary-purple": "#511de1",
 			}
 		},
 	},
