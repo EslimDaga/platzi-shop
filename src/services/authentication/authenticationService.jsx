414c09b90beb39f79856d2de65a75db5d186fc4b
Eslim Daga <esliim9811@gmail.com>
2023-04-17 22:57:06 -0500

    feat:⭐:Eh agregado la petición getProfile, para saber los datos del usuario que inicio sesión.

--- src/services/authentication/authenticationService.jsx
+++ src/services/authentication/authenticationService.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { API_URL } from "../../constants";
+import jwtInterceptor from "../jwtInterceptor";
 
 export const signInService = async (credentials) => {
   const response = await axios.post(`${API_URL}/auth/login/`, credentials);
   return response.data;
 }
+
+export const getProfileService = async () => {
+  const response = await jwtInterceptor.get(`${API_URL}/auth/profile/`);
+  return response.data;
+};
