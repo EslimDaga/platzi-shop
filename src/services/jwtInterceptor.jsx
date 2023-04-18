8df1215e9e3031a7cf3bfc8033d0594be4c06c9c
Eslim Daga <esliim9811@gmail.com>
2023-04-17 22:13:02 -0500

    config:🔧:Eh dejado la configuración inicial.

---
+++ src/services/jwtInterceptor.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const jwtInterceptor = axios.create({
+  baseURL: "https://api.escuelajs.co/api/v1/",
+  headers: {
+    Authorization: "Bearer ",
+  },
+});
+
+jwtInterceptor.interceptors.request.use(
+  (config) => {
+    let token = JSON.parse(localStorage.getItem("user"));
+    config.headers.Authorization = `Bearer ${token.access}`;
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+)
+
+jwtInterceptor.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("user");
+      console.log("401");
+    }
+
+    if (error.response.status === 403) {
+      console.log("403");
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default jwtInterceptor;
