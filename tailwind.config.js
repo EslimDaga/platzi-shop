757f74bc74b48f67d6b375cd5f9092693930aedd
Eslim Daga <esliim9811@gmail.com>
2023-04-18 19:57:43 -0500

    update:💄:Eh agregado un color más a la configuración de tailwind css como también a los estilos principales

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
 			},
 			colors: {
 				"platzi-primary-background": "#13161c",
+				"platzi-secondary-background": "#20252f",
+				"platzi-terciary-background": "#2D3543",
 				"platzi-primary-green": "#0ae98a",
 				"platzi-primary-purple": "#4d1bd1",
 				"platzi-secondary-purple": "#511de1",
