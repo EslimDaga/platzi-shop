17fd6d43917508dbd77fc6dc532e05343146ec62
Eslim Daga <esliim9811@gmail.com>
2023-04-18 21:32:46 -0500

    update:➕:Eh agregado el ProductsReducer en el app

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import ProductsReducer from "../features/products/productsSlice";
 import AuthenticationReducer from "../features/authentication/authenticationSlice";
 
 const store = configureStore({
   reducer: {
-    authentication: AuthenticationReducer
+    authentication: AuthenticationReducer,
+    products: ProductsReducer,
   },
   middleware: [thunk, logger],
 });
