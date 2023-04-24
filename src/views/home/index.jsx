37611f577404000759238770fcba5b90c449ed2f
Eslim Daga <esliim9811@gmail.com>
2023-04-24 17:28:47 -0500

    update:➖:Eh eliminado el menu en el header

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,26 +1,17 @@
-import {
-  Bars3Icon,
-  ChartPieIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { getProducts } from "../../features/products/productsSlice";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Dialog, Transition } from "@headlessui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { Fragment, useEffect, useState } from "react";
-import { CheckIcon, ChevronDownIcon, HeartIcon, ShoppingBagIcon, TrashIcon } from "@heroicons/react/20/solid";
-import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import { CheckIcon, HeartIcon, ShoppingBagIcon, TrashIcon } from "@heroicons/react/20/solid";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const Home = () => {
   const [openBag, setOpenBag] = useState(false);
   const [bagTotal, setBagTotal] = useState(0);
   const [bagProducts, setBagProducts] = useState([]);
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -72,70 +63,8 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="w-6 h-6" aria-hidden="true" />
-            </button>
           </div>
-          <Popover.Group className="hidden lg:flex lg:gap-x-12">
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
-              Inicio
-            </a>
-            <Popover className="relative">
-              <Popover.Button className="flex items-center text-sm font-semibold leading-6 text-white outline-none gap-x-1">
-                Categorías
-                <ChevronDownIcon
-                  className="flex-none w-5 h-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </Popover.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-1"
-              >
-                <Popover.Panel className="absolute z-10 w-screen max-w-md mt-3 overflow-hidden shadow-lg -left-8 top-full rounded-3xl bg-platzi-secondary-background ring-1 ring-gray-900/5">
-                  <div className="p-4">
-                    <div className="relative flex items-center p-4 text-sm leading-6 group gap-x-6 rounded-xl hover:bg-platzi-primary-background">
-                      <div className="flex items-center justify-center flex-none h-11 w-11 rounded-xl bg-platzi-primary-background group-hover:bg-platzi-secondary-background">
-                        <ChartPieIcon
-                          className="w-6 h-6 text-gray-600 group-hover:text-platzi-primary-green"
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <div className="flex-auto">
-                        <a href="#" className="block font-semibold text-white">
-                          Analytics
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1 text-gray-100">
-                          Get a better understanding of your traffic
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </Popover>
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
-              Delivery
-            </a>
-          </Popover.Group>
           <div className="items-center hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link
-              to="/iniciar-sesion"
-              className="text-sm font-semibold leading-6 text-white"
-            >
-              Iniciar sesión <span aria-hidden="true">&rarr;</span>
-            </Link>
             <div
               className="flex flex-row p-2 px-4 truncate rounded cursor-pointer"
               onClick={() => { setOpenBag(!openBag); }}
@@ -152,85 +81,6 @@ const Home = () => {
             </div>
           </div>
         </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-10" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full px-6 py-6 overflow-y-auto bg-platzi-terciary-background sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">platzi</span>
-                <img
-                  className="w-auto h-8"
-                  src="/images/logo_platzi.png"
-                  alt="logo_platzi"
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-100"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="w-6 h-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="flow-root mt-6">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="py-6 space-y-2">
-                  <a
-                    href="#"
-                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-white rounded-lg hover:bg-platzi-secondary-background"
-                  >
-                    Inicio
-                  </a>
-                  <Disclosure as="div" className="-mx-3">
-                    {({ open }) => (
-                      <>
-                        <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-platzi-secondary-background">
-                          Productos
-                          <ChevronDownIcon
-                            className={classNames(
-                              open ? "rotate-180" : "",
-                              "h-5 w-5 flex-none"
-                            )}
-                            aria-hidden="true"
-                          />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className="mt-2 space-y-2">
-                          <Disclosure.Button
-                            as="a"
-                            href="#"
-                            className="block py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white rounded-lg hover:bg-platzi-secondary-background"
-                          >
-                            Analytics
-                          </Disclosure.Button>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                  <a
-                    href="#"
-                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-white rounded-lg hover:bg-platzi-secondary-background"
-                  >
-                    Delivery
-                  </a>
-                </div>
-                <div className="py-6">
-                  <Link
-                    to="/iniciar-sesion"
-                    className="-mx-3 block bg-platzi-primary-purple rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-platzi-secondary-background"
-                  >
-                    Iniciar sesión
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
       </header>
       <section className="px-8 py-5 mt-20">
         <Transition.Root show={openBag} as={Fragment}>
